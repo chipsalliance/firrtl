0b84230d25c662ced4020b7804932670020a465f
azidar <azidar@gmail.com>
2016-08-19 16:58:34 -0700

    Added ReplaceSubAccesses before RemoveSubAccesses

---
+++ src/main/scala/firrtl/passes/ReplaceSubAccess.scala
@@ -0,0 +1,30 @@
+package firrtl.passes
+
+import firrtl.ir._
+import firrtl.{WRef, WSubAccess, WSubIndex, WSubField}
+import firrtl.Mappers._
+import firrtl.Utils._
+import firrtl.WrappedExpression._
+import firrtl.Namespace
+import scala.collection.mutable
+
+
+/** Removes all [[firrtl.WSubAccess]] from circuit
+  */
+object ReplaceAccesses extends Pass {
+  def name = "Replace Accesses"
+
+  def run(c: Circuit): Circuit = {
+    def onStmt(s: Statement): Statement = s map onStmt map onExp
+    def onExp(e: Expression): Expression = e match {
+      case WSubAccess(e, UIntLiteral(value, width), t, g) => WSubIndex(e, value.toInt, t, g)
+      case e => e map onExp
+    }
+  
+    val newModules = c.modules.map( _ match {
+      case m: ExtModule => m
+      case Module(i, n, ps, b) => Module(i, n, ps, onStmt(b))
+    })
+    Circuit(c.info, newModules, c.main)
+  }
+}
