7f0b49873024b0a8e182885edf1869da3d8270fc
azidar <azidar@gmail.com>
2016-10-19 11:43:23 -0700

    Added wiring pass and simple test

---
+++ src/main/scala/firrtl/passes/wiring/Wiring.scala
@@ -0,0 +1,290 @@
+package firrtl.passes
+package wiring
+
+import firrtl._
+import firrtl.ir._
+import firrtl.Utils._
+import firrtl.Mappers._
+import scala.collection.mutable
+import firrtl.Annotations._
+import WiringUtils._
+
+/** A component, e.g. register etc.
+  */
+case class SourceAnnotation(target: ComponentName, tID: TransID) extends Annotation with Loose with Unstable {
+  def duplicate(n: Named) = n match {
+    case n: ComponentName => this copy (target = n)
+  }
+}
+
+/** A module, e.g. ExtModule etc.
+  */
+case class SinkAnnotation(target: ModuleName, tID: TransID) extends Annotation with Loose with Unstable {
+  def duplicate(n: Named) = n match {
+    case n: ModuleName => this copy (target = n)
+  }
+}
+
+case class WiringInfo(src: ComponentName, sinks: Set[ModuleName], top: ModuleName)
+
+case class Lineage(
+  name: String,
+  children: Seq[(String, Lineage)] = Seq.empty,
+  source: Boolean = false,
+  sink: Boolean = false,
+  sourceParent: Boolean = false,
+  sinkParent: Boolean = false,
+  sharedParent: Boolean = false,
+  addPort: Option[(String, DecType)] = None,
+  cons: Seq[(String, String)] = Seq.empty
+) {
+  def map(f: Lineage => Lineage): Lineage =
+    this.copy(children = children.map(c => (c._1, f(c._2))))
+  override def toString: String = smallString("")
+  def smallString(tab: String): String = s"""
+$tab name: $name,
+$tab children: ${children.map(c => tab + "   " + c._2.smallString(tab + "    "))}"""
+  def serialize(tab: String): String = s"""
+$tab name: $name,
+$tab source: $source,
+$tab sink: $sink,
+$tab sourceParent: $sourceParent,
+$tab sinkParent: $sinkParent,
+$tab sharedParent: $sharedParent,
+$tab addPort: $addPort
+$tab cons: $cons
+$tab children: ${children.map(c => tab + "   " + c._2.serialize(tab + "    "))}"""
+}
+
+trait DecType
+case object DecInput extends DecType
+case object DecOutput extends DecType
+case object DecWire extends DecType
+
+class Wiring(wi: WiringInfo) extends Pass {
+  def name = this.getClass.getSimpleName
+  private def toExp(s: String): Expression = {
+    val tokens = s.split('.')
+    val root: Expression = WRef(tokens.head, UnknownType, ExpKind, UNKNOWNGENDER)
+    tokens.tail.foldLeft(root){(e, s) =>
+      WSubField(e, s, UnknownType, UNKNOWNGENDER)
+    }
+  }
+  private def onModule(map: Map[String, Lineage], t: Type)(m: DefModule) = {
+    map.get(m.name) match {
+      case None => m
+      case Some(l) =>
+        val stmts = mutable.ArrayBuffer[Statement]()
+        val ports = mutable.ArrayBuffer[Port]()
+        l.addPort match {
+          case None =>
+          case Some((s, dt)) => dt match {
+            case DecInput => ports += Port(NoInfo, s, Input, t)
+            case DecOutput => ports += Port(NoInfo, s, Output, t)
+            case DecWire =>
+              stmts += DefWire(NoInfo, s, t)
+          }
+        }
+        stmts ++= l.cons.map{case ((l, r)) => 
+          Connect(NoInfo, toExp(l), toExp(r))
+        }
+        def onStmt(s: Statement): Statement = Block(Seq(s) ++ stmts)
+        m match {
+          case Module(i, n, ps, s) => Module(i, n, ps ++ ports, Block(Seq(s) ++ stmts))
+          case ExtModule(i, n, ps) => ExtModule(i, n, ps ++ ports)
+        }
+    }
+  }
+  private def getType(c: Circuit, module: String, comp: String) = {
+    var tpe: Option[Type] = None
+    def getType(s: Statement): Statement = s match {
+      case DefRegister(_, n, t, _, _, _) if n == comp =>
+        tpe = Some(t)
+        s
+      case sx => sx map getType
+    }
+    val m =  c.modules.filter(_.name == module) match {
+      case Seq(mod) => mod
+      case _ => error("Must have a module named $module")
+    }
+    m match {
+      case Module(i, n, ps, b) => getType(b)
+      case e: ExtModule => error("Module $module cannot be an external module")
+    }
+    tpe match {
+      case None => error("Didn't find register $comp in $module!")
+      case Some(t) => t
+    }
+  }
+  def run(c: Circuit): Circuit = {
+    /* check
+    there is only one source in shared parent top
+    there are no other instance sinks in modules not in scope of wiring pass
+    component is a ref (not index, or access, or subfield, or anything)
+    */
+    val source = wi.src.module.name
+    val sinks = wi.sinks.map(_.name)
+    val compName = wi.src.name
+    val portNames = c.modules.foldLeft(Map[String, String]()){(map, m) =>
+      map + (m.name -> Namespace(m).newName(wi.src.name))
+    }
+    val lineages = getLineage(c, wi.top.name)
+
+    println(s"""Source Module: $source""")
+    println(s"""Sink Modules: $sinks""")
+    println(s"""Lineages: ${lineages.smallString("")}""")
+
+    val withFields = setSharedParent(wi.top.name)(setFields(sinks, source)(lineages))
+    println(s"""Lineages with fields: $withFields""")
+
+    val withThings = setThings(portNames, compName)(withFields)
+    println(s"""Lineages with things: $withThings""")
+
+    val map = pointToLineage(withThings)
+    println(s"""Map: $map""")
+    val cx = c.copy(modules = c.modules map onModule(map, getType(c, source, compName)))
+    println(cx.serialize)
+    cx
+  }
+}
+
+
+object WiringUtils {
+  type ChildrenMap = mutable.HashMap[String, Seq[(String, String)]]
+  def getChildrenMap(c: Circuit): ChildrenMap = {
+    val childrenMap = new ChildrenMap()
+    def getChildren(mname: String)(s: Statement): Statement = s match {
+      case s: WDefInstance => 
+        childrenMap(mname) = childrenMap(mname) :+ (s.name, s.module)
+        s
+      case s: DefInstance => 
+        childrenMap(mname) = childrenMap(mname) :+ (s.name, s.module)
+        s
+      case s => s map getChildren(mname)
+    }
+    c.modules.foreach{ m =>
+      childrenMap(m.name) = Nil
+      m map getChildren(m.name)
+    }
+    childrenMap
+  }
+
+  def getLineage(c: Circuit, top: String): Lineage = {
+    val childrenMap = getChildrenMap(c)
+    getLineage(childrenMap, top)
+  }
+
+  def getLineage(childrenMap: ChildrenMap, module: String): Lineage =
+    Lineage(module, childrenMap(module) map (c => (c._1, getLineage(childrenMap, c._2))))
+
+  def setFields(sinks: Set[String], source: String)(lin: Lineage): Lineage = lin map setFields(sinks, source) match {
+    case l if sinks.contains(l.name) => l.copy(sink = true)
+    case l if l.name == source => l.copy(source = true, sourceParent = true)
+    case l => 
+      val sinkParent = l.children.foldLeft(false){(b, c) => b || c._2.sink || c._2.sinkParent}
+      val sourceParent = l.children.foldLeft(false){(b, c) => b || c._2.source || c._2.sourceParent}
+      l.copy(sinkParent=sinkParent, sourceParent=sourceParent)
+  }
+
+  def setSharedParent(top: String)(lin: Lineage): Lineage = lin map setSharedParent(top) match {
+    case l if l.name == top => l.copy(sharedParent = true)
+    case l => l
+  }
+
+  def setThings(portNames:Map[String, String], compName: String)(lin: Lineage): Lineage = {
+    val funs = Seq(
+      ((l: Lineage) => l map setThings(portNames, compName)),
+      ((l: Lineage) => l match {
+        case Lineage(name, _, _, _, _, _, true, _, _) => //SharedParent
+          l.copy(addPort=Some((portNames(name), DecWire)))
+        case Lineage(name, _, _, _, true, _, _, _, _) => //SourceParent
+          l.copy(addPort=Some((portNames(name), DecOutput)))
+        case Lineage(name, _, _, _, _, true, _, _, _) => //SinkParent
+          l.copy(addPort=Some((portNames(name), DecInput)))
+        case Lineage(name, _, _, true, _, _, _, _, _) => //Sink
+          l.copy(addPort=Some((portNames(name), DecInput)))
+        case l => l
+      }),
+      ((l: Lineage) => l match {
+        case Lineage(name, _, true, _, _, _, _, _, _) => //Source
+          val tos = Seq(s"${portNames(name)}")
+          val from = compName
+          l.copy(cons=l.cons ++ tos.map(t => (t, from)))
+        case Lineage(name, _, _, _, true, _, _, _, _) => //SourceParent
+          val tos = Seq(s"${portNames(name)}")
+          val from = l.children.filter{case (i, c) => c.sourceParent}.map{case (i, c) => s"$i.${portNames(c.name)}"}.head
+          l.copy(cons=l.cons ++ tos.map(t => (t, from)))
+        case l => l
+      }),
+      ((l: Lineage) => l match {
+        case Lineage(name, _, _, _, _, true, _, _, _) => //SinkParent
+          val tos = l.children.filter{case (i, c) => (c.sinkParent || c.sink) && !c.sourceParent}.map{case (i, c) => s"$i.${portNames(c.name)}"}
+          val from = s"${portNames(name)}"
+          l.copy(cons=l.cons ++ tos.map(t => (t, from)))
+        case l => l
+      })
+    )
+    funs.foldLeft(lin)((l, fun) => fun(l))
+  }
+
+  def pointToLineage(lin: Lineage): Map[String, Lineage] = {
+    val map = mutable.HashMap[String, Lineage]()
+    def onLineage(l: Lineage): Lineage = {
+      map(l.name) = l
+      l map onLineage
+    }
+    onLineage(lin)
+    map.toMap
+  }
+
+  //type ModuleMap = Map[String, DefModule]
+  //def getModuleMap(c: Circuit): ModuleMap =
+  //  c.modules.foldLeft(Map[String, DefModule]()){ (map, m) => map + (m.name -> m) }
+
+  //def getSharedParent(lineages: Seq[Lineage]): Option[(String, String)] = {
+  //  require(lineages.size > 0)
+  //  val maxDepth = lineages.map(_.size).reduce(math.min(_, _))
+  //  def findSharedParents(depth: Int): Option[(String, String)] = {
+  //    if(depth == maxDepth) {
+  //      Some(lineages.head(depth - 1))
+  //    } else {
+  //      val siblings = lineages.map(l => l(depth))
+  //      siblings.forall(_ == siblings.head) match {
+  //        case true => findSharedParents(depth + 1)
+  //        case false if depth == 0 => None
+  //        case false => Some(lineages.head(depth - 1))
+  //      }
+  //    }
+  //  }
+  //  findSharedParents(0)
+  //}
+
+  //def countInstances(parentMap: ParentMap, top: String)(module: String): Int =
+  //  getAllParents(parentMap)(module).filter(_ == top).size
+
+  //def inLineage(lineage: Lineage, parent: String): Boolean = 
+  //  lineage.map(_ == parent).foldLeft(false)(_ || _)
+
+  //def inAllLineages(lineages: Seq[Lineage], parent: String): Boolean = 
+  //  lineages.foldLeft(true)((prevIn, lineage) => prevIn && inLineage(lineage, parent))
+
+
+  //def getLineage(parentMap: ParentMap, parent: String)(child: String): Set[String] =
+  //  getAllParents(parentMap)(child) - getAllParents(parentMap)(parent) - parent
+
+  //def countInstances(childrenMap: ChildrenMap): Map[String, Int] = {
+  //  val countChildren = mutable.HashMap[String, (String, Int)]()
+  //  childrenMap.keys.foreach{ parent =>
+  //    childrenMap(parent).foreach { child =>
+  //      countChildren(parent) = 
+  //    }
+  //  }
+
+  //  val parents = parentMap(child)
+  //  parents.size match {
+  //    case 0 => 1
+  //    case n => parents map countInstances(parentMap) reduce (_ + _)
+
+
+  //}
+}
---
+++ src/test/scala/firrtlTests/WiringTests.scala
@@ -0,0 +1,119 @@
+package firrtlTests
+
+import java.io._
+import org.scalatest._
+import org.scalatest.prop._
+import firrtl._
+import firrtl.ir.Circuit
+import firrtl.passes._
+import firrtl.Parser.IgnoreInfo
+import Annotations._
+import wiring.WiringUtils._
+import wiring._
+
+class WiringTests extends FirrtlFlatSpec {
+  def parse (input:String) = Parser.parse(input.split("\n").toIterator, IgnoreInfo)
+  private def executeTest(input: String, expected: Seq[String], passes: Seq[Pass]) = {
+    val c = passes.foldLeft(Parser.parse(input.split("\n").toIterator)) {
+      (c: Circuit, p: Pass) => p.run(c)
+    }
+    val lines = c.serialize.split("\n") map normalized
+
+    expected foreach { e =>
+      lines should contain(e)
+    }
+  }
+
+  def passes = Seq(
+    ToWorkingIR,
+    ResolveKinds,
+    InferTypes,
+    ResolveGenders,
+    InferWidths
+  )
+
+  "Wiring from r to X" should "work" in {
+    val cName = CircuitName("Top")
+    val source = ComponentName("r", ModuleName("C", cName))
+    val sinks = Set(ModuleName("X", cName))
+    val sas = WiringInfo(source, sinks, ModuleName("A", cName))
+    val input =
+      """circuit Top :
+        |  module Top :
+        |    input clk: Clock
+        |    inst a of A
+        |    a.clk <= clk
+        |  module A :
+        |    input clk: Clock
+        |    inst b of B
+        |    b.clk <= clk
+        |    inst x of X
+        |    x.clk <= clk
+        |  module B :
+        |    input clk: Clock
+        |    inst c of C
+        |    c.clk <= clk
+        |    inst d of D
+        |    d.clk <= clk
+        |  module C :
+        |    input clk: Clock
+        |    reg r: UInt<5>, clk
+        |  module D :
+        |    input clk: Clock
+        |    inst x1 of X
+        |    x1.clk <= clk
+        |    inst x2 of X
+        |    x2.clk <= clk
+        |  extmodule X :
+        |    input clk: Clock
+        |""".stripMargin
+    val check =
+      """circuit Top :
+        |  module Top :
+        |    input clk: Clock
+        |    inst a of A
+        |    a.clk <= clk
+        |  module A :
+        |    input clk: Clock
+        |    inst b of B
+        |    b.clk <= clk
+        |    inst x of X
+        |    x.clk <= clk
+        |    wire r: UInt<5>
+        |    r <= b.r
+        |    x.r <= r
+        |  module B :
+        |    input clk: Clock
+        |    output r: UInt<5>
+        |    inst c of C
+        |    c.clk <= clk
+        |    inst d of D
+        |    d.clk <= clk
+        |    r <= c.r_0
+        |    d.r <= r
+        |  module C :
+        |    input clk: Clock
+        |    output r_0: UInt<5>
+        |    reg r: UInt<5>, clk
+        |    r_0 <= r
+        |  module D :
+        |    input clk: Clock
+        |    input r: UInt<5>
+        |    inst x1 of X
+        |    x1.clk <= clk
+        |    inst x2 of X
+        |    x2.clk <= clk
+        |    x1.r <= r
+        |    x2.r <= r
+        |  extmodule X :
+        |    input clk: Clock
+        |    input r: UInt<5>
+        |""".stripMargin
+    val c = passes.foldLeft(parse(input)) {
+      (c: Circuit, p: Pass) => p.run(c)
+    }
+    val wiringPass = new Wiring(sas)
+    val retC = wiringPass.run(c)
+    (parse(retC.serialize).serialize) should be (parse(check).serialize)
+  }
+}
