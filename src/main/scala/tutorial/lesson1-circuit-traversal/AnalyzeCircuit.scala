85fb4d64fa23bfe3bbcca91bd7c9202bf9e1e8f0
Adam Izraelevitz <azidar@gmail.com>
2017-03-13 16:14:47 -0700

    Removed comments for picture

--- src/main/scala/tutorial/lesson1-circuit-traversal/AnalyzeCircuit.scala
+++ src/main/scala/tutorial/lesson1-circuit-traversal/AnalyzeCircuit.scala
@@ -69,73 +69,29 @@ class Ledger {
   *   - https://github.com/ucb-bar/firrtl/wiki/Common-Pass-Idioms
   */
 class AnalyzeCircuit extends Transform {
-  // Requires the [[Circuit]] form to be "low"
   def inputForm = LowForm
-  // Indicates the output [[Circuit]] form to be "low"
   def outputForm = LowForm
-
-  // Called by [[Compiler]] to run your pass. [[CircuitState]] contains
-  // the circuit and its form, as well as other related data.
   def execute(state: CircuitState): CircuitState = {
     val ledger = new Ledger()
     val circuit = state.circuit
-
-    // Execute the function walkModule(ledger) on every [[DefModule]] in
-    // circuit, returning a new [[Circuit]] with new [[Seq]] of [[DefModule]].
-    //   - "higher order functions" - using a function as an object
-    //   - "function currying" - partial argument notation
-    //   - "infix notation" - fancy function calling syntax
-    //   - "map" - classic functional programming concept
-    //   - discard the returned new [[Circuit]] because circuit is unmodified
     circuit map walkModule(ledger)
-
-    // Print our ledger
-    println(ledger.serialize)
-
-    // Return an unchanged [[CircuitState]]
+    println(ledger.serialize) // Print our ledger
     state
   }
-
-  // Deeply visits every [[Statement]] in m.
   def walkModule(ledger: Ledger)(m: DefModule): DefModule = {
-    // Set ledger to current module name
     ledger.setModuleName(m.name)
-
-    // Execute the function walkStatement(ledger) on every [[Statement]] in m.
-    //   - return the new [[DefModule]] (in this case, its identical to m)
-    //   - if m does not contain [[Statement]], map returns m.
     m map walkStatement(ledger)
   }
-
-  // Deeply visits every [[Statement]] and [[Expression]] in s.
   def walkStatement(ledger: Ledger)(s: Statement): Statement = {
-
-    // Execute the function walkExpression(ledger) on every [[Expression]] in s.
-    //   - discard the new [[Statement]] (in this case, its identical to s)
-    //   - if s does not contain [[Expression]], map returns s.
     s map walkExpression(ledger)
-
-    // Execute the function walkStatement(ledger) on every [[Statement]] in s.
-    //   - return the new [[Statement]] (in this case, its identical to s)
-    //   - if s does not contain [[Statement]], map returns s.
     s map walkStatement(ledger)
   }
-
-  // Deeply visits every [[Expression]] in e.
-  //   - "post-order traversal" - handle e's children [[Expression]] before e
   def walkExpression(ledger: Ledger)(e: Expression): Expression = {
-
-    // Execute the function walkExpression(ledger) on every [[Expression]] in e.
-    //   - return the new [[Expression]] (in this case, its identical to e)
-    //   - if s does not contain [[Expression]], map returns e.
     val visited = e map walkExpression(ledger)
-
     visited match {
-      // If e is a [[Mux]], increment our ledger and return e.
       case Mux(cond, tval, fval, tpe) =>
         ledger.foundMux
         e
-      // If e is not a [[Mux]], return e.
       case notmux => notmux
     }
   }
