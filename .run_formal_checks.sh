1108e95b6af650dc126b8e7d34d43bee2a3ed651
Jack Koenig <jack.koenig3@gmail.com>
2017-12-16 11:37:58 -0800

    [skip formal checks] Travis experiment

--- .run_formal_checks.sh
+++ .run_formal_checks.sh
@@ -8,6 +8,9 @@ fi
 
 DUT=$1
 
+echo "TRAVIS_COMMIT_MESSAGE"
+echo $TRAVIS_COMMIT_MESSAGE
+
 # Run formal check only for PRs
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
     echo "Not a pull request, no formal check"
