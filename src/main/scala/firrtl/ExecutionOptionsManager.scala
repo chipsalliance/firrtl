9609ee48b194ffebc23a87583606d09fcfc66d89
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-04-12 22:53:09 -0400

    Define an explicit `AugmentedOptionParser`
    This moves the custom modifications to scopt.OptionParser into an explicit
    subclass.
    
    Note: I attempted to do this with a trait, but it wasn't working. The
    motivation for this (the inability of unidoc to pick up the type of the
    modified parser) may be suspect.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/ExecutionOptionsManager.scala
+++ src/main/scala/firrtl/ExecutionOptionsManager.scala
@@ -14,6 +14,26 @@ import java.io.File
 import net.jcazevedo.moultingyaml._
 import firrtl.annotations.AnnotationYamlProtocol._
 
+/** A modified [[scopt.OptionParser]] with mutable termination and additional checks
+  *
+  * @param name the name of the application
+  */
+sealed class AugmentedOptionParser(name: String) extends OptionParser[AnnotationSeq](name) {
+  var terminateOnExit = true
+  override def terminate(exitState: Either[String, Unit]): Unit =
+    if (terminateOnExit) sys.exit(0)
+
+  /* Check that no long or short option declarations are duplicated */
+  override def parse(args: Seq[String], init: AnnotationSeq): Option[AnnotationSeq] = {
+    val longDups = options.map(_.name).groupBy(identity).collect{ case (k, v) if v.size > 1 && k != "" => k }
+    val shortDups = options.map(_.shortOpt).flatten.groupBy(identity).collect{ case (k, v) if v.size > 1 => k }
+    def msg(x: String, y: String) = s"""Duplicate $x "$y" (did your custom Transform or OptionsManager add this?)"""
+    if (longDups.nonEmpty)  { throw new FIRRTLException(msg("long option", shortDups.map("--" + _).mkString(","))) }
+    if (shortDups.nonEmpty) { throw new FIRRTLException(msg("short option", shortDups.map("-" + _).mkString(","))) }
+    super.parse(args, init)
+  }
+}
+
 /** A store of command line options as an [[AnnotationSeq]]
   *
   * @param applicationName  The name shown in the usage
@@ -25,23 +45,8 @@ class ExecutionOptionsManager(
   args: Array[String],
   annotations: AnnotationSeq = AnnotationSeq(Seq.empty)) {
 
-  /** An [[scopt.OptionParser]] used by subclasses of [[ExecutionOptionsManager]] */
-  final val parser = new OptionParser[AnnotationSeq](applicationName) {
-    var terminateOnExit = true
-    override def terminate(exitState: Either[String, Unit]): Unit = {
-      if(terminateOnExit) sys.exit(0)
-    }
-
-    /* Check that no long or short option declarations are duplicated */
-    override def parse(args: Seq[String], init: AnnotationSeq): Option[AnnotationSeq] = {
-      val longDups = options.map(_.name).groupBy(identity).collect{ case (k, v) if v.size > 1 && k != "" => k }
-      val shortDups = options.map(_.shortOpt).flatten.groupBy(identity).collect{ case (k, v) if v.size > 1 => k }
-      def msg(x: String, y: String) = s"""Duplicate $x "$y" (did your custom Transform or OptionsManager add this?)"""
-      if (longDups.nonEmpty)  { throw new FIRRTLException(msg("long option", shortDups.map("--" + _).mkString(","))) }
-      if (shortDups.nonEmpty) { throw new FIRRTLException(msg("short option", shortDups.map("-" + _).mkString(","))) }
-      super.parse(args, init)
-    }
-  }
+  /** Command line argument parser ([[scopt.OptionParser]]) with modifications */
+  final val parser = new AugmentedOptionParser(applicationName)
 
   /** By default scopt calls sys.exit when --help is in options, this defeats that */
   def doNotExitOnHelp(): Unit = {
