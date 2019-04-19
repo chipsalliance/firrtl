51454966ada0b8454837734014dc942464951507
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-04-19 10:44:50 -0400

    Use PhaseManager in FirrtlStage
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/stage/FirrtlStage.scala
+++ src/main/scala/firrtl/stage/FirrtlStage.scala
@@ -3,7 +3,7 @@
 package firrtl.stage
 
 import firrtl.{AnnotationSeq, CustomTransformException, FIRRTLException, Utils}
-import firrtl.options.{Stage, Phase, PhaseException, Shell, OptionsException, StageMain}
+import firrtl.options.{Phase, PhaseException, PhaseManager, PreservesAll, Shell, Stage, OptionsException, StageMain}
 import firrtl.options.phases.DeletedWrapper
 import firrtl.passes.{PassException, PassExceptions}
 
@@ -11,17 +11,12 @@ import scala.util.control.ControlThrowable
 
 import java.io.PrintWriter
 
-class FirrtlStage extends Stage {
+class FirrtlStage extends Stage with PreservesAll[Phase] {
+
   val shell: Shell = new Shell("firrtl") with FirrtlCli
 
-  private val phases: Seq[Phase] =
-    Seq( new firrtl.stage.phases.AddDefaults,
-         new firrtl.stage.phases.AddImplicitEmitter,
-         new firrtl.stage.phases.Checks,
-         new firrtl.stage.phases.AddCircuit,
-         new firrtl.stage.phases.AddImplicitOutputFile,
-         new firrtl.stage.phases.Compiler,
-         new firrtl.stage.phases.WriteEmitted )
+  val phases: Seq[Phase] = new PhaseManager(Set(classOf[firrtl.stage.phases.WriteEmitted]))
+      .transformOrder
       .map(DeletedWrapper(_))
 
   def run(annotations: AnnotationSeq): AnnotationSeq = try {
