ca0bcc8eff587dd1fc341862c09243bace64ea44
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-04-19 14:23:52 -0400

    Use PhaseManager in Driver
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/Driver.scala
+++ src/main/scala/firrtl/Driver.scala
@@ -2,7 +2,6 @@
 
 package firrtl
 
-import scala.collection._
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
 import scala.util.control.ControlThrowable
@@ -18,7 +17,7 @@ import firrtl.transforms._
 import firrtl.Utils.throwInternalError
 import firrtl.stage.{FirrtlExecutionResultView, FirrtlStage}
 import firrtl.stage.phases.DriverCompatibility
-import firrtl.options.{StageUtils, Phase, Viewer}
+import firrtl.options.{Phase, PhaseManager, StageUtils, Viewer}
 import firrtl.options.phases.DeletedWrapper
 
 
@@ -216,13 +215,18 @@ object Driver {
 
     val annos = optionsManager.firrtlOptions.toAnnotations ++ optionsManager.commonOptions.toAnnotations
 
-    val phases: Seq[Phase] =
-      Seq( new DriverCompatibility.AddImplicitAnnotationFile,
-           new DriverCompatibility.AddImplicitFirrtlFile,
-           new DriverCompatibility.AddImplicitOutputFile,
-           new DriverCompatibility.AddImplicitEmitter,
-           new FirrtlStage )
+    val phases: Seq[Phase] = {
+      import DriverCompatibility._
+      new PhaseManager(
+        Set( classOf[AddImplicitAnnotationFile],
+             classOf[AddImplicitFirrtlFile],
+             classOf[AddImplicitOutputFile],
+             classOf[AddImplicitEmitter],
+             classOf[FirrtlStage] )
+          .map(_.asInstanceOf[Class[Phase]]) )
+        .transformOrder
         .map(DeletedWrapper(_))
+    }
 
     val annosx = try {
       phases.foldLeft(annos)( (a, p) => p.transform(a) )
