a82f3835ed6c347488e4df97cd7171df86c841f2
colin4124 <colin4124@gmail.com>
2019-06-28 09:04:52 +0800

    Refactor AddDefaultsSpec

--- src/test/scala/firrtlTests/options/phases/AddDefaultsSpec.scala
+++ src/test/scala/firrtlTests/options/phases/AddDefaultsSpec.scala
@@ -4,35 +4,24 @@ package firrtlTests.options.phases
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import firrtl.AnnotationSeq
-import firrtl.options.{OptionsException, Phase, TargetDirAnnotation}
+import firrtl.options.{Phase, TargetDirAnnotation}
 import firrtl.options.phases.AddDefaults
 
 class AddDefaultsSpec extends FlatSpec with Matchers {
 
-  val targetDir = TargetDirAnnotation("foo")
-
-  class Fixture { val phase: Phase = new AddDefaults }
+  class Fixture {
+    val phase: Phase = new AddDefaults
+    val targetDir = TargetDirAnnotation("foo")
+    val defaultDir = TargetDirAnnotation(".")
+  }
 
   behavior of classOf[AddDefaults].toString
 
   it should "add a TargetDirAnnotation if it does not exist" in new Fixture {
-    val Occurrences = phase.transform(Seq.empty) count {
-      a => a match {
-        case _: TargetDirAnnotation => true
-        case _ => false
-      }
-    }
-    assert(Occurrences == 1)
+    phase.transform(Seq.empty).toSeq should be (Seq(defaultDir))
   }
 
   it should "don't add a TargetDirAnnotation if it exists" in new Fixture {
-    val Occurrences = phase.transform(Seq(targetDir)) count {
-      a => a match {
-        case _: TargetDirAnnotation => true
-        case _ => false
-      }
-    }
-    assert(Occurrences == 1)
+    phase.transform(Seq(targetDir)).toSeq should be (Seq(targetDir))
   }
 }
