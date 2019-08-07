220ba15287010dd270bcbb5c9de848c30f4c333e
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-08-07 00:14:20 -0400

    Improve RemoveReset handling of invalid inits
    This modifies RemoveReset to NOT generate a mux for
    invalid (IsInvalid) inits. In the case of an invalid init, the reset
    is converted to a self-connect and no mux is generated.
    
    This is implemented as a new, initial pass over the module to populate
    a set of all invalid signals. During the subsequent, circuit-modifying
    pass, this invalid set is queried to special case the handling of
    invalid inits.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/transforms/RemoveReset.scala
+++ src/main/scala/firrtl/transforms/RemoveReset.scala
@@ -5,6 +5,7 @@ package transforms
 
 import firrtl.ir._
 import firrtl.Mappers._
+import firrtl.WrappedExpression.we
 
 import scala.collection.mutable
 
@@ -20,13 +21,22 @@ class RemoveReset extends Transform {
 
   private def onModule(m: DefModule): DefModule = {
     val resets = mutable.HashMap.empty[String, Reset]
+    val initToReset = mutable.HashMap.empty[WrappedExpression, String]
     def onStmt(stmt: Statement): Statement = {
       stmt match {
         case reg @ DefRegister(_, rname, _, _, reset, init)
             if reset != Utils.zero && reset.tpe != AsyncResetType =>
           // Add register reset to map
           resets(rname) = Reset(reset, init)
+          initToReset(we(init)) = rname
           reg.copy(reset = Utils.zero, init = WRef(reg))
+        /* If we come across an invalidated expression used as an initialization for a reset, then remove that register
+         * from pending, later mux generation. We can do this because the IR is SSA and the invalidation must come
+         * before its use. WARNING: this is modifying state in "resets", but it saves an additional traversal.
+         */
+        case inv @ IsInvalid(_, expr) if initToReset.contains(we(expr)) =>
+          resets -= initToReset(we(expr))
+          inv
         case Connect(info, ref @ WRef(rname, _, RegKind, _), expr) if resets.contains(rname) =>
           val reset = resets(rname)
           val muxType = Utils.mux_type_and_widths(reset.value, expr)
