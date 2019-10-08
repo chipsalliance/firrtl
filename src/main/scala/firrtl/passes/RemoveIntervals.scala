a88abc075b3e91484e05a556a13b8ca3c456a25b
Adam Izraelevitz <azidar@gmail.com>
2019-10-08 16:32:24 -0700

    Modified RemoveIntervals to pass UniquifySpec

--- src/main/scala/firrtl/passes/RemoveIntervals.scala
+++ src/main/scala/firrtl/passes/RemoveIntervals.scala
@@ -42,90 +42,94 @@ class RemoveIntervals extends Pass {
 
   private def replaceStmtInterval(s: Statement): Statement = s map replaceTypeInterval map replaceStmtInterval map replaceExprInterval
 
-  private def replaceExprInterval(e: Expression): Expression = e map replaceExprInterval match {
-    case DoPrim(AsInterval, Seq(a1), _, tpe) => DoPrim(AsSInt, Seq(a1), Seq.empty, tpe)
-    case DoPrim(BPShl, args, consts, tpe) => DoPrim(Shl, args, consts, tpe)
-    case DoPrim(BPShr, args, consts, tpe) => DoPrim(Shr, args, consts, tpe)
-    case DoPrim(Clip, Seq(a1, _), Nil, tpe: IntervalType) =>
-      // Output interval (pre-calculated)
-      val clipLo = tpe.minAdjusted.get
-      val clipHi = tpe.maxAdjusted.get
-      // Input interval
-      val (inLow, inHigh) = a1.tpe match {
-        case t2: IntervalType => (t2.minAdjusted.get, t2.maxAdjusted.get)
-        case _ => sys.error("Shouldn't be here")
-      }
-      val gtOpt = clipHi >= inHigh
-      val ltOpt = clipLo <= inLow
-      (gtOpt, ltOpt) match {
-        // input range within output range -> no optimization
-        case (true, true)  => a1
-        case (true, false) => Mux(Lt(a1, clipLo.S), clipLo.S, a1)
-        case (false, true) => Mux(Gt(a1, clipHi.S), clipHi.S, a1)
-        case _             => Mux(Gt(a1, clipHi.S), clipHi.S, Mux(Lt(a1, clipLo.S), clipLo.S, a1))
-      }
+  private def replaceExprInterval(e: Expression): Expression = e match {
+    case _: WRef | _: WSubIndex | _: WSubField => e
+    case o =>
+      o map replaceExprInterval match {
+        case DoPrim(AsInterval, Seq(a1), _, tpe) => DoPrim(AsSInt, Seq(a1), Seq.empty, tpe)
+        case DoPrim(BPShl, args, consts, tpe) => DoPrim(Shl, args, consts, tpe)
+        case DoPrim(BPShr, args, consts, tpe) => DoPrim(Shr, args, consts, tpe)
+        case DoPrim(Clip, Seq(a1, _), Nil, tpe: IntervalType) =>
+          // Output interval (pre-calculated)
+          val clipLo = tpe.minAdjusted.get
+          val clipHi = tpe.maxAdjusted.get
+          // Input interval
+          val (inLow, inHigh) = a1.tpe match {
+            case t2: IntervalType => (t2.minAdjusted.get, t2.maxAdjusted.get)
+            case _ => sys.error("Shouldn't be here")
+          }
+          val gtOpt = clipHi >= inHigh
+          val ltOpt = clipLo <= inLow
+          (gtOpt, ltOpt) match {
+            // input range within output range -> no optimization
+            case (true, true) => a1
+            case (true, false) => Mux(Lt(a1, clipLo.S), clipLo.S, a1)
+            case (false, true) => Mux(Gt(a1, clipHi.S), clipHi.S, a1)
+            case _ => Mux(Gt(a1, clipHi.S), clipHi.S, Mux(Lt(a1, clipLo.S), clipLo.S, a1))
+          }
 
-    case DoPrim(Squeeze, Seq(a1, a2), Nil, tpe: IntervalType) =>
-      // Using (conditional) reassign interval w/o adding mux
-      val a2tpe = a2.tpe.asInstanceOf[IntervalType]
-      val a1tpe = a1.tpe.asInstanceOf[IntervalType]
-      val min2 = a2tpe.min.get * BigDecimal(BigInt(1) << a1tpe.point.get.toInt)
-      // Conservative
-      val minOpt2 = min2.setScale(0, FLOOR).toBigInt
-      val max2 = a2tpe.max.get * BigDecimal(BigInt(1) << a1tpe.point.get.toInt)
-      val maxOpt2 = max2.setScale(0, CEILING).toBigInt
-      val w2 = Seq(minOpt2.bitLength, maxOpt2.bitLength).max + 1
-      val w1 = Seq(a1tpe.minAdjusted.get.bitLength, a1tpe.maxAdjusted.get.bitLength).max + 1
-      if (w1 < w2)
-        a1
-      else {
-        val bits = DoPrim(Bits, Seq(a1), Seq(w2 - 1, 0), UIntType(IntWidth(w2)))
-        DoPrim(AsSInt, Seq(bits), Seq.empty, SIntType(IntWidth(w2)))
-      }
-    case DoPrim(Wrap, Seq(a1, a2), Nil, tpe: IntervalType) => a2.tpe match {
-      // If a2 type is Interval wrap around range. If UInt, wrap around width
-      case t: IntervalType =>
-        // Need to match binary points before getting *adjusted!
-        val (wrapLo, wrapHi) = t.copy(point = tpe.point) match {
-          case t: IntervalType => (t.minAdjusted.get, t.maxAdjusted.get)
-          case _ => Utils.throwInternalError(s"Illegal AST state: cannot have $e not have an IntervalType")
-        }
-        val (inLo, inHi) = a1.tpe match {
-          case t2: IntervalType => (t2.minAdjusted.get, t2.maxAdjusted.get)
+        case DoPrim(Squeeze, Seq(a1, a2), Nil, tpe: IntervalType) =>
+          // Using (conditional) reassign interval w/o adding mux
+          val a2tpe = a2.tpe.asInstanceOf[IntervalType]
+          val a1tpe = a1.tpe.asInstanceOf[IntervalType]
+          val min2 = a2tpe.min.get * BigDecimal(BigInt(1) << a1tpe.point.get.toInt)
+          // Conservative
+          val minOpt2 = min2.setScale(0, FLOOR).toBigInt
+          val max2 = a2tpe.max.get * BigDecimal(BigInt(1) << a1tpe.point.get.toInt)
+          val maxOpt2 = max2.setScale(0, CEILING).toBigInt
+          val w2 = Seq(minOpt2.bitLength, maxOpt2.bitLength).max + 1
+          val w1 = Seq(a1tpe.minAdjusted.get.bitLength, a1tpe.maxAdjusted.get.bitLength).max + 1
+          if (w1 < w2)
+            a1
+          else {
+            val bits = DoPrim(Bits, Seq(a1), Seq(w2 - 1, 0), UIntType(IntWidth(w2)))
+            DoPrim(AsSInt, Seq(bits), Seq.empty, SIntType(IntWidth(w2)))
+          }
+        case DoPrim(Wrap, Seq(a1, a2), Nil, tpe: IntervalType) => a2.tpe match {
+          // If a2 type is Interval wrap around range. If UInt, wrap around width
+          case t: IntervalType =>
+            // Need to match binary points before getting *adjusted!
+            val (wrapLo, wrapHi) = t.copy(point = tpe.point) match {
+              case t: IntervalType => (t.minAdjusted.get, t.maxAdjusted.get)
+              case _ => Utils.throwInternalError(s"Illegal AST state: cannot have $e not have an IntervalType")
+            }
+            val (inLo, inHi) = a1.tpe match {
+              case t2: IntervalType => (t2.minAdjusted.get, t2.maxAdjusted.get)
+              case _ => sys.error("Shouldn't be here")
+            }
+            // If (max input) - (max wrap) + (min wrap) is less then (maxwrap), we can optimize when (max input > max wrap)
+            val range = wrapHi - wrapLo
+            val ltOpt = Add(a1, (range + 1).S)
+            val gtOpt = Sub(a1, (range + 1).S)
+            // [Angie]: This is dangerous. Would rather throw compilation error right now than allow "Rem" without the user explicitly including it.
+            // If x < wl
+            // output: wh - (wl - x) + 1 AKA x + r + 1
+            // worst case: wh - (wl - xl) + 1 = wl
+            // -> xl + wr + 1 = wl
+            // If x > wh
+            // output: wl + (x - wh) - 1 AKA x - r - 1
+            // worst case: wl + (xh - wh) - 1 = wh
+            // -> xh - wr - 1 = wh
+            val default = Add(Rem(Sub(a1, wrapLo.S), Sub(wrapHi.S, wrapLo.S)), wrapLo.S)
+            (wrapHi >= inHi, wrapLo <= inLo, (inHi - range - 1) <= wrapHi, (inLo + range + 1) >= wrapLo) match {
+              case (true, true, _, _) => a1
+              case (true, _, _, true) => Mux(Lt(a1, wrapLo.S), ltOpt, a1)
+              case (_, true, true, _) => Mux(Gt(a1, wrapHi.S), gtOpt, a1)
+              // Note: inHi - range - 1 = wrapHi can't be true when inLo + range + 1 = wrapLo (i.e. simultaneous extreme cases don't work)
+              case (_, _, true, true) => Mux(Gt(a1, wrapHi.S), gtOpt, Mux(Lt(a1, wrapLo.S), ltOpt, a1))
+              case _ =>
+                println(
+                  s" wrapHi >= inHi: ${wrapHi >= inHi} ; " +
+                    s"wrapLo <= inLo: ${wrapLo <= inLo} ; " +
+                    s"(inHi - range - 1) <= wrapHi: ${(inHi - range - 1) <= wrapHi} ; " +
+                    s"(inLo + range + 1) >= wrapLo: ${(inLo + range + 1) >= wrapLo}"
+                )
+                sys.error(s"Wraps with remainder currently unsupported. in: $inLo , $inHi ; out: $wrapLo , $wrapHi") // default
+            }
           case _ => sys.error("Shouldn't be here")
         }
-        // If (max input) - (max wrap) + (min wrap) is less then (maxwrap), we can optimize when (max input > max wrap)
-        val range = wrapHi - wrapLo
-        val ltOpt = Add(a1, (range + 1).S)
-        val gtOpt = Sub(a1, (range + 1).S)
-        // [Angie]: This is dangerous. Would rather throw compilation error right now than allow "Rem" without the user explicitly including it.   
-        // If x < wl
-        // output: wh - (wl - x) + 1 AKA x + r + 1
-        // worst case: wh - (wl - xl) + 1 = wl   
-        // -> xl + wr + 1 = wl
-        // If x > wh
-        // output: wl + (x - wh) - 1 AKA x - r - 1
-        // worst case: wl + (xh - wh) - 1 = wh
-        // -> xh - wr - 1 = wh
-        val default = Add(Rem(Sub(a1, wrapLo.S), Sub(wrapHi.S, wrapLo.S)), wrapLo.S)
-        (wrapHi >= inHi, wrapLo <= inLo, (inHi - range - 1) <= wrapHi, (inLo + range + 1) >= wrapLo) match {
-          case (true, true, _, _) => a1
-          case (true, _, _, true) => Mux(Lt(a1, wrapLo.S), ltOpt, a1)
-          case (_, true, true, _) => Mux(Gt(a1, wrapHi.S), gtOpt, a1)
-          // Note: inHi - range - 1 = wrapHi can't be true when inLo + range + 1 = wrapLo (i.e. simultaneous extreme cases don't work)
-          case (_, _, true, true) => Mux(Gt(a1, wrapHi.S), gtOpt, Mux(Lt(a1, wrapLo.S), ltOpt, a1))
-          case _                          => 
-            println(
-              s" wrapHi >= inHi: ${wrapHi >= inHi} ; " +
-              s"wrapLo <= inLo: ${wrapLo <= inLo} ; " + 
-              s"(inHi - range - 1) <= wrapHi: ${(inHi - range - 1) <= wrapHi} ; " + 
-              s"(inLo + range + 1) >= wrapLo: ${(inLo + range + 1) >= wrapLo}"
-            )                       
-            sys.error(s"Wraps with remainder currently unsupported. in: $inLo , $inHi ; out: $wrapLo , $wrapHi") // default
-        }
-      case _ => sys.error("Shouldn't be here")
-    }
-    case other => other
+        case other => other
+      }
   }
 
   private def replacePortInterval(p: Port): Port = p map replaceTypeInterval
