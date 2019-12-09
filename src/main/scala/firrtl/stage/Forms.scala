abc86341962597e97a6f26ab7bf55fe6c4f7b415
Albert Magyar <albert.magyar@gmail.com>
2019-12-09 11:46:15 -0800

    Replace Forms defs with lazy vals for efficiency

--- src/main/scala/firrtl/stage/Forms.scala
+++ src/main/scala/firrtl/stage/Forms.scala
@@ -14,17 +14,17 @@ import firrtl.stage.TransformManager.TransformDependency
 
 object Forms {
 
-  def ChirrtlForm: Seq[TransformDependency] = Seq.empty
+  lazy val ChirrtlForm: Seq[TransformDependency] = Seq.empty
 
-  def MinimalHighForm: Seq[TransformDependency] = ChirrtlForm ++
+  lazy val MinimalHighForm: Seq[TransformDependency] = ChirrtlForm ++
     Seq( DependencyID[passes.CheckChirrtl],
          DependencyID[passes.CInferTypes],
          DependencyID[passes.CInferMDir],
          DependencyID[passes.RemoveCHIRRTL] )
 
-  def WorkingIR: Seq[TransformDependency] = MinimalHighForm :+ DependencyID[passes.ToWorkingIR]
+  lazy val WorkingIR: Seq[TransformDependency] = MinimalHighForm :+ DependencyID[passes.ToWorkingIR]
 
-  def Resolved: Seq[TransformDependency] = WorkingIR ++
+  lazy val Resolved: Seq[TransformDependency] = WorkingIR ++
     Seq( DependencyID[passes.CheckHighForm],
          DependencyID[passes.ResolveKinds],
          DependencyID[passes.InferTypes],
@@ -38,15 +38,15 @@ object Forms {
          DependencyID[passes.CheckWidths],
          DependencyID[firrtl.transforms.InferResets] )
 
-  def Deduped: Seq[TransformDependency] = Resolved :+ DependencyID[firrtl.transforms.DedupModules]
+  lazy val Deduped: Seq[TransformDependency] = Resolved :+ DependencyID[firrtl.transforms.DedupModules]
 
-  def HighForm: Seq[TransformDependency] = ChirrtlForm ++
+  lazy val HighForm: Seq[TransformDependency] = ChirrtlForm ++
     MinimalHighForm ++
     WorkingIR ++
     Resolved ++
     Deduped
 
-  def MidForm: Seq[TransformDependency] = HighForm ++
+  lazy val MidForm: Seq[TransformDependency] = HighForm ++
     Seq( DependencyID[passes.PullMuxes],
          DependencyID[passes.ReplaceAccesses],
          DependencyID[passes.ExpandConnects],
@@ -56,7 +56,7 @@ object Forms {
          DependencyID[passes.ConvertFixedToSInt],
          DependencyID[passes.ZeroWidth] )
 
-  def LowForm: Seq[TransformDependency] = MidForm ++
+  lazy val LowForm: Seq[TransformDependency] = MidForm ++
     Seq( DependencyID(passes.LowerTypes),
          DependencyID[passes.Legalize],
          DependencyID[firrtl.transforms.RemoveReset],
@@ -64,19 +64,19 @@ object Forms {
          DependencyID[checks.CheckResets],
          DependencyID[firrtl.transforms.RemoveWires] )
 
-  def LowFormMinimumOptimized: Seq[TransformDependency] = LowForm ++
+  lazy val LowFormMinimumOptimized: Seq[TransformDependency] = LowForm ++
     Seq( DependencyID[passes.RemoveValidIf],
          DependencyID[passes.memlib.VerilogMemDelays],
          DependencyID[passes.SplitExpressions] )
 
-  def LowFormOptimized: Seq[TransformDependency] = LowFormMinimumOptimized ++
+  lazy val LowFormOptimized: Seq[TransformDependency] = LowFormMinimumOptimized ++
     Seq( DependencyID[firrtl.transforms.ConstantPropagation],
          DependencyID[passes.PadWidths],
          DependencyID[firrtl.transforms.CombineCats],
          DependencyID[passes.CommonSubexpressionElimination],
          DependencyID[firrtl.transforms.DeadCodeElimination] )
 
-  def VerilogMinimumOptimized: Seq[TransformDependency] = LowFormMinimumOptimized ++
+  lazy val VerilogMinimumOptimized: Seq[TransformDependency] = LowFormMinimumOptimized ++
     Seq( DependencyID[firrtl.transforms.BlackBoxSourceHelper],
          DependencyID[firrtl.transforms.ReplaceTruncatingArithmetic],
          DependencyID[firrtl.transforms.FlattenRegUpdate],
@@ -85,7 +85,7 @@ object Forms {
          DependencyID[passes.VerilogPrep],
          DependencyID[firrtl.AddDescriptionNodes] )
 
-  def VerilogOptimized: Seq[TransformDependency] = LowFormOptimized ++
+  lazy val VerilogOptimized: Seq[TransformDependency] = LowFormOptimized ++
     Seq( DependencyID[firrtl.transforms.BlackBoxSourceHelper],
          DependencyID[firrtl.transforms.ReplaceTruncatingArithmetic],
          DependencyID[firrtl.transforms.FlattenRegUpdate],
