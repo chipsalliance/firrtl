5b21be0bc1ac9ed09f96cde47114e200a1c803f4
Jack Koenig <koenig@sifive.com>
2020-03-13 23:45:53 -0700

    Use HashMap instead of LinkedHashMap in ResolveKinds
    * Backport to 1.2.x
    
    (cherry picked from commit 0f78e2df9d8f03e6847a012873fc2408b62d0692)
    
    Co-authored-by: Albert Magyar <albert.magyar@gmail.com>

--- src/main/scala/firrtl/passes/ResolveKinds.scala
+++ src/main/scala/firrtl/passes/ResolveKinds.scala
@@ -7,6 +7,41 @@ import firrtl.ir._
 import firrtl.Mappers._
 
 object ResolveKinds extends Pass {
+  private def find_port(kinds: collection.mutable.HashMap[String, Kind])(p: Port): Port = {
+    kinds(p.name) = PortKind ; p
+  }
+
+  def find_stmt(kinds: collection.mutable.HashMap[String, Kind])(s: Statement):Statement = {
+    s match {
+      case sx: DefWire => kinds(sx.name) = WireKind
+      case sx: DefNode => kinds(sx.name) = NodeKind
+      case sx: DefRegister => kinds(sx.name) = RegKind
+      case sx: WDefInstance => kinds(sx.name) = InstanceKind
+      case sx: DefMemory => kinds(sx.name) = MemKind
+      case _ =>
+    }
+    s map find_stmt(kinds)
+  }
+
+  def resolve_expr(kinds: collection.mutable.HashMap[String, Kind])(e: Expression): Expression = e match {
+    case ex: WRef => ex copy (kind = kinds(ex.name))
+    case _ => e map resolve_expr(kinds)
+  }
+
+  def resolve_stmt(kinds: collection.mutable.HashMap[String, Kind])(s: Statement): Statement =
+    s map resolve_stmt(kinds) map resolve_expr(kinds)
+
+
+  def resolve_kinds(m: DefModule): DefModule = {
+    val kinds = new collection.mutable.HashMap[String, Kind]
+    (m map find_port(kinds)
+       map find_stmt(kinds)
+       map resolve_stmt(kinds))
+  }
+
+  def run(c: Circuit): Circuit =
+    c copy (modules = c.modules map resolve_kinds)
+
   type KindMap = collection.mutable.LinkedHashMap[String, Kind]
 
   def find_port(kinds: KindMap)(p: Port): Port = {
@@ -32,14 +67,4 @@ object ResolveKinds extends Pass {
 
   def resolve_stmt(kinds: KindMap)(s: Statement): Statement =
     s map resolve_stmt(kinds) map resolve_expr(kinds)
-
-  def resolve_kinds(m: DefModule): DefModule = {
-    val kinds = new KindMap
-    (m map find_port(kinds)
-       map find_stmt(kinds)
-       map resolve_stmt(kinds))
-  }
-
-  def run(c: Circuit): Circuit =
-    c copy (modules = c.modules map resolve_kinds)
 }
