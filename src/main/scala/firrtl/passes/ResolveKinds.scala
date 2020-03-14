677dc577c6a198a0b4a781eacff384f6c1b539ce
Jack Koenig <koenig@sifive.com>
2020-03-13 23:46:31 -0700

    Eliminate unnecessary traversals in ResolveKinds
    * Modify for backporting

--- src/main/scala/firrtl/passes/ResolveKinds.scala
+++ src/main/scala/firrtl/passes/ResolveKinds.scala
@@ -11,7 +11,12 @@ object ResolveKinds extends Pass {
     kinds(p.name) = PortKind ; p
   }
 
-  def find_stmt(kinds: collection.mutable.HashMap[String, Kind])(s: Statement):Statement = {
+  def resolve_expr(kinds: collection.mutable.HashMap[String, Kind])(e: Expression): Expression = e match {
+    case ex: WRef => ex copy (kind = kinds(ex.name))
+    case _ => e map resolve_expr(kinds)
+  }
+
+  def resolve_stmt(kinds: collection.mutable.HashMap[String, Kind])(s: Statement): Statement = {
     s match {
       case sx: DefWire => kinds(sx.name) = WireKind
       case sx: DefNode => kinds(sx.name) = NodeKind
@@ -20,23 +25,14 @@ object ResolveKinds extends Pass {
       case sx: DefMemory => kinds(sx.name) = MemKind
       case _ =>
     }
-    s map find_stmt(kinds)
+    s.map(resolve_stmt(kinds))
+     .map(resolve_expr(kinds))
   }
-
-  def resolve_expr(kinds: collection.mutable.HashMap[String, Kind])(e: Expression): Expression = e match {
-    case ex: WRef => ex copy (kind = kinds(ex.name))
-    case _ => e map resolve_expr(kinds)
-  }
-
-  def resolve_stmt(kinds: collection.mutable.HashMap[String, Kind])(s: Statement): Statement =
-    s map resolve_stmt(kinds) map resolve_expr(kinds)
-
 
   def resolve_kinds(m: DefModule): DefModule = {
     val kinds = new collection.mutable.HashMap[String, Kind]
-    (m map find_port(kinds)
-       map find_stmt(kinds)
-       map resolve_stmt(kinds))
+    m.map(find_port(kinds))
+    m.map(resolve_stmt(kinds))
   }
 
   def run(c: Circuit): Circuit =
@@ -68,3 +64,4 @@ object ResolveKinds extends Pass {
   def resolve_stmt(kinds: KindMap)(s: Statement): Statement =
     s map resolve_stmt(kinds) map resolve_expr(kinds)
 }
+
