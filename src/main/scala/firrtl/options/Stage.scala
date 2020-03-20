20e6ad5bf2a58c92b0e7f4a1e11d40e54b971961
Jim Lawson <ucbjrl@berkeley.edu>
2020-03-19 17:11:34 -0700

    Respond to comments - make _dummyForms private.

--- src/main/scala/firrtl/options/Stage.scala
+++ src/main/scala/firrtl/options/Stage.scala
@@ -63,7 +63,7 @@ abstract class Stage extends Phase {
   */
 class StageMain(val stage: Stage) {
   // Force initialization of the Forms object - https://github.com/freechipsproject/firrtl/issues/1462
-  val _dummyForms = firrtl.stage.Forms
+  private val _dummyForms = firrtl.stage.Forms
   /** The main function that serves as this stage's command line interface.
     * @param args command line arguments
     */
