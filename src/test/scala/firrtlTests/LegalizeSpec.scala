dacd4fe33cf06402e8487128dfebcf32b5996734
Albert Magyar <albert.magyar@gmail.com>
2020-03-23 20:36:07 -0700

    Add Yosys test of formerly broken pad(cast(lit)) circuit

--- src/test/scala/firrtlTests/LegalizeSpec.scala
+++ src/test/scala/firrtlTests/LegalizeSpec.scala
@@ -2,7 +2,20 @@
 
 package firrtlTests
 
-import firrtl.testutils.ExecutionTest
+import firrtl.passes.Legalize
+import firrtl.testutils.{ExecutionTest, FirrtlFlatSpec}
 
 class LegalizeExecutionTest extends ExecutionTest("Legalize", "/passes/Legalize")
 
+class LegalizeEquivalenceSpec extends FirrtlFlatSpec {
+  "Legalize" should "handle casts of literals" in {
+    val input =
+      s"""circuit literalsel_fir:
+         |  module literalsel_fir:
+         |    input u1: UInt<4>
+         |    output o: UInt<8>
+         |    o <= asUInt(bits(dshl(pad(asSInt(UInt<2>("h1")), 8), asUInt(u1)), 7, 0))
+         |""".stripMargin
+    firrtlEquivalenceTest(input, Nil)
+  }
+}
