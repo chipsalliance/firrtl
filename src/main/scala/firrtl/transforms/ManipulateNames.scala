5d0771243511bd725a3bafe21e39b2fc205ac25b
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2020-06-01 15:06:27 -0400

    Revert "Add features.{LowerCaseNames, UpperCaseNames} transforms"
    This reverts commit c8dcdacf313f19a4d0238be694478a325432edd4.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/transforms/ManipulateNames.scala
+++ src/main/scala/firrtl/transforms/ManipulateNames.scala
@@ -6,7 +6,9 @@ import firrtl._
 import firrtl.analyses.InstanceGraph
 import firrtl.annotations.{Named, CircuitName, ModuleName, ComponentName}
 import firrtl.Mappers._
+import firrtl.options.Dependency
 import firrtl.passes.PassException
+import firrtl.stage.Forms
 
 import scala.collection.mutable
 
