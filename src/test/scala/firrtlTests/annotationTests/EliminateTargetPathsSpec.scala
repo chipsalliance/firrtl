f3e8639bb6e33c636292f3e789f19bed1f13f2f3
Jack Koenig <koenig@sifive.com>
2020-06-10 12:47:44 -0700

    Build ArrayBuffers in Block.mapStmt (#1669)
    * Build ArrayBuffers in Block.mapStmt
    
    * Have empty Block serialize as "skip"
    
    The FIRRTL parser requires at least one indented line in each module.
    Sometimes tests emit and parse modules with no contents; this ensures
    there's always at least a "skip" in empty modules.
    
    Also fix tests that expected certain skips
    
    * Use var List as stack in Block.mapStmt impl
    
    This replaces Iterator concatenation. In Scala 2.11, RHS recursion on
    Iterators is not stack safe. This seems to have been fixed in 2.12 by
    Scala PR 5033.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit 1e497ce39fd161c681ee64c0a4a882859eb8850b)
    
    # Conflicts:
    #	src/test/scala/firrtlTests/annotationTests/EliminateTargetPathsSpec.scala
    #	src/test/scala/firrtlTests/annotationTests/MorphismSpec.scala

--- src/test/scala/firrtlTests/annotationTests/EliminateTargetPathsSpec.scala
+++ src/test/scala/firrtlTests/annotationTests/EliminateTargetPathsSpec.scala
@@ -378,10 +378,21 @@ class EliminateTargetPathsSpec extends FirrtlPropSpec with FirrtlMatchers {
       """|circuit Foo:
          |  module Bar:
          |    node x = UInt<1>(0)
-         |    skip
          |  module Foo:
          |    inst bar of Bar
          |    inst baz of Bar""".stripMargin
+<<<<<<< HEAD
+=======
+    val check =
+      """|circuit Foo:
+         |  module Bar___Foo_bar:
+         |    node x = UInt<1>(0)
+         |  module Bar:
+         |    node x = UInt<1>(0)
+         |  module Foo:
+         |    inst bar of Bar___Foo_bar
+         |    inst baz of Bar""".stripMargin
+>>>>>>> 1e497ce3... Build ArrayBuffers in Block.mapStmt (#1669)
     val Bar_x = CircuitTarget("Foo").module("Bar").ref("x")
     val output = CircuitState(passes.ToWorkingIR.run(Parser.parse(input)), UnknownForm, Seq(DontTouchAnnotation(Bar_x)))
       .resolvePaths(Seq(CircuitTarget("Foo").module("Foo").instOf("bar", "Bar")))
@@ -406,7 +417,6 @@ class EliminateTargetPathsSpec extends FirrtlPropSpec with FirrtlMatchers {
          |  module Bar:
          |    node foo = UInt<1>(0)
          |    inst baz of Baz
-         |    skip
          |  module Foo:
          |    node foo = UInt<1>(0)
          |    inst bar of Bar
