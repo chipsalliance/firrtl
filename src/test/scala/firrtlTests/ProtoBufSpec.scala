b2eb66fa2746d0fa5a48f72fb1e3187f44ce8d2c
Jack Koenig <koenig@sifive.com>
2020-06-23 13:41:38 -0700

    Fixup ProtoBuf ValidIf test for 1.2.x

--- src/test/scala/firrtlTests/ProtoBufSpec.scala
+++ src/test/scala/firrtlTests/ProtoBufSpec.scala
@@ -5,11 +5,7 @@ package firrtlTests
 import firrtl.FirrtlProtos.Firrtl
 import firrtl._
 import firrtl.ir._
-<<<<<<< HEAD
-=======
-import firrtl.testutils._
 import firrtl.Utils.BoolType
->>>>>>> d1db9067... Add support for ValidIf to ProtoBuf [de]serialization
 
 class ProtoBufSpec extends FirrtlFlatSpec {
 
@@ -207,11 +203,7 @@ class ProtoBufSpec extends FirrtlFlatSpec {
   }
 
   it should "support ValidIf" in {
-    val en = ir.Reference("en", BoolType, PortKind, SourceFlow)
-    val value = ir.Reference("x", UIntType(IntWidth(8)), PortKind, SourceFlow)
-    val vi = ir.ValidIf(en, value, value.tpe)
-    // Deserialized has almost nothing filled in
-    val expected = ir.ValidIf(ir.Reference("en"), ir.Reference("x"), UnknownType)
-    FromProto.convert(ToProto.convert(vi).build) should equal (expected)
+    val vi = ir.ValidIf(ir.Reference("en", UnknownType), ir.Reference("x", UnknownType), UnknownType)
+    FromProto.convert(ToProto.convert(vi).build) should equal (vi)
   }
 }
