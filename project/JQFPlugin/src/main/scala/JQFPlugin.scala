bdd62f3bbd71904dfbbf3df9fb6bd028358b86cc
Albert Chen <albert.chen@sifive.com>
2020-06-27 12:54:51 -0700

    add documentation to JQFPlugin

--- project/JQFPlugin/src/main/scala/JQFPlugin.scala
+++ project/JQFPlugin/src/main/scala/JQFPlugin.scala
@@ -11,46 +11,51 @@ case class JQFException(message: String) extends Exception(message)
 
 object JQFPlugin extends AutoPlugin {
   object autoImport {
-    val jqfOutputDirectory = settingKey[File]("")
+    val jqfOutputDirectory = settingKey[File](
+      "The the output directory where fuzzing results will be stored. " +
+        "Defaults to ${compileTargetDir}/JQF")
 
-    val jqfInputDirectory = settingKey[Option[File]]("")
+    val jqfInputDirectory = settingKey[Option[File]](
+      "The name of the input directory containing seed files. " +
+        "If not provided, then fuzzing starts with randomly generated initial inputs.")
 
     val jqfProject = settingKey[Project](
-      "Project containing the JQF main class, JQF tasks will use the runMain task of this project")
+      "Project containing the JQF main class. JQF tasks will use the runMain task of this project")
 
     val jqfFuzzMainClass = settingKey[String](
-      "The main class to run for the JQF Fuzz task")
+      "The main class to run for the JQF Fuzz task. " +
+        "Must accept --inputDirectory, --outputDirectory, and --classpathElements arguments")
 
-    val jqfExtraArguments = settingKey[Seq[String]](
-      "Additional command-line arguments to pass on every mdoc invocation. " +
-        "For example, add '--no-link-hygiene' to disable link hygiene."
+    val jqfFuzzExtraArguments = settingKey[Seq[String]](
+      "Additional command-line arguments to pass on every jqfFuzz invocation. "
     )
 
-    val jqfFuzz = inputKey[Unit]("")
+    val jqfFuzz = inputKey[Unit](
+      "Runs the jqfFuzzMainClass in the jqfProject. " +
+        "Supplies the '--classpathElements' argument with the current project's classpath")
   }
 
   import autoImport._
 
-
   override val trigger: PluginTrigger = noTrigger
 
   override val requires: Plugins = plugins.JvmPlugin
-  
+
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     jqfOutputDirectory := target.in(Compile).value / "JQF",
 
     jqfInputDirectory := None,
 
-    jqfExtraArguments := Seq.empty,
-    
+    jqfFuzzExtraArguments := Seq.empty,
+
     jqfFuzz := (Def.inputTaskDyn {
-      val extraArgs = jqfExtraArguments.value
+      val extraArgs = jqfFuzzExtraArguments.value
       val parsed = spaceDelimited("<arg>").parsed
       val defaultArgs = toCmdlineArgs.value
       val args = (defaultArgs ++ parsed).mkString(" ")
+      val project = jqfProject.value
+      val main = jqfFuzzMainClass.value
       Def.taskDyn {
-        val project = jqfProject.value
-        val main = jqfFuzzMainClass.value
         (project/runMain).in(Compile).toTask(s" $main $args")
       }
     }).evaluated,
