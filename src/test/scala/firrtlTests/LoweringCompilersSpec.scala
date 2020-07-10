eb46713242dc3676eb78df7b308823503385c6fc
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2020-07-10 16:33:57 -0400

    Remove overlapping inputForm=LowForm tests
    Remove tests from LoweringCompilerSpec testing the placement of
    inputForm=LowForm legacy, custom transforms. This behavior is already
    tested in the CustomTransformSpec.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>
    (cherry picked from commit f5dd8ddc72ce829406337f22e73cac27b58aa521)
    
    # Conflicts:
    #	src/test/scala/firrtlTests/LoweringCompilersSpec.scala

--- src/test/scala/firrtlTests/LoweringCompilersSpec.scala
+++ src/test/scala/firrtlTests/LoweringCompilersSpec.scala
@@ -8,7 +8,7 @@ import firrtl._
 import firrtl.passes
 import firrtl.options.Dependency
 import firrtl.stage.{Forms, TransformManager}
-import firrtl.transforms.{IdentityTransform, LegalizeAndReductionsTransform}
+import firrtl.transforms.IdentityTransform
 
 sealed trait PatchAction { val line: Int }
 
@@ -329,30 +329,6 @@ class LoweringCompilersSpec extends FlatSpec with Matchers {
         Seq(new Transforms.LowToChirrtl) ++
         (new TransformManager(Forms.LowFormOptimized, Forms.ChirrtlForm).flattenedTransformOrder)
     val tm = new TransformManager(Forms.LowFormOptimized :+ Dependency[Transforms.LowToChirrtl])
-    compare(expected, tm)
-  }
-
-  it should "schedule inputForm=LowForm after MiddleFirrtlToLowFirrtl for the LowFirrtlEmitter" in {
-    val expected =
-      new TransformManager(Forms.LowForm).flattenedTransformOrder ++
-        Seq(new Transforms.LowToLow, new firrtl.LowFirrtlEmitter)
-    val tm = (new TransformManager(Seq(Dependency[firrtl.LowFirrtlEmitter], Dependency[Transforms.LowToLow])))
-    compare(expected, tm)
-  }
-
-  it should "schedule inputForm=LowForm after MinimumLowFirrtlOptimizations for the MinimalVerilogEmitter" in {
-    val expected =
-      new TransformManager(Forms.LowFormMinimumOptimized).flattenedTransformOrder ++
-        Seq(new Transforms.LowToLow, new LegalizeAndReductionsTransform, new firrtl.MinimumVerilogEmitter)
-    val tm = (new TransformManager(Seq(Dependency[firrtl.MinimumVerilogEmitter], Dependency[Transforms.LowToLow])))
-    compare(expected, tm)
-  }
-
-  it should "schedule inputForm=LowForm after LowFirrtlOptimizations for the VerilogEmitter" in {
-    val expected =
-      new TransformManager(Forms.LowFormOptimized).flattenedTransformOrder ++
-        Seq(new Transforms.LowToLow, new LegalizeAndReductionsTransform, new firrtl.VerilogEmitter)
-    val tm = (new TransformManager(Seq(Dependency[firrtl.VerilogEmitter], Dependency[Transforms.LowToLow])))
     compare(expected, tm)
   }
 
