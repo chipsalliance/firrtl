af894fee13a1b1a503ed26d844c9c9366136cb13
tampler <socnetfpga@gmail.com>
2020-07-29 22:07:40 +0300

    Initial 2.12 integration

--- src/main/scala/firrtl/options/OptionParser.scala
+++ src/main/scala/firrtl/options/OptionParser.scala
@@ -23,29 +23,3 @@ trait DoNotTerminateOnExit { this: OptionParser[_] =>
 trait ExceptOnError { this: OptionParser[_] =>
   override def reportError(msg: String): Unit = throw new OptionsException(msg)
 }
-
-/** A modified OptionParser with mutable termination and additional checks
-  */
-trait DuplicateHandling extends OptionParser[AnnotationSeq] {
-
-  override def parse(args: Seq[String], init: AnnotationSeq): Option[AnnotationSeq] = {
-
-    /** Message for found duplicate options */
-    def msg(x: String, y: String) = s"""Duplicate $x "$y" (did your custom Transform or OptionsManager add this?)"""
-
-    val longDups = options.map(_.name).groupBy(identity).collect{ case (k, v) if v.size > 1 && k != "" => k }
-    val shortDups = options.map(_.shortOpt).flatten.groupBy(identity).collect{ case (k, v) if v.size > 1 => k }
-
-
-    if (longDups.nonEmpty)  {
-      throw new OptionsException(msg("long option", longDups.map("--" + _).mkString(",")), new IllegalArgumentException)
-    }
-
-    if (shortDups.nonEmpty) {
-      throw new OptionsException(msg("short option", shortDups.map("-" + _).mkString(",")), new IllegalArgumentException)
-    }
-
-    super.parse(args, init)
-  }
-
-}
