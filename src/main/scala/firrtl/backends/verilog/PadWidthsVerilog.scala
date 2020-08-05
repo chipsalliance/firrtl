98b880546d4ee300e70e9fb895836a0c607a51fe
Kevin Laeufer <kevin.laeufer@sifive.com>
2020-08-05 13:15:36 -0700

    PadWidths: split into Verilog specific part

---
+++ src/main/scala/firrtl/backends/verilog/PadWidthsVerilog.scala
@@ -0,0 +1,51 @@
+// See LICENSE for license details.
+
+package firrtl.backends.verilog
+
+import firrtl.ir._
+import firrtl.PrimOps._
+import firrtl.Mappers._
+import firrtl.{Dshlw, SystemVerilogEmitter, Transform, VerilogEmitter, bitWidth}
+import firrtl.options.Dependency
+import firrtl.stage.Forms
+import firrtl.stage.TransformManager.TransformDependency
+
+
+/** Adds padding for mul, div, rem, dshl which breaks firrtl width invariance, but is nmeeded to match Verilog semantics */
+object PadWidthsVerilog extends firrtl.passes.Pass {
+
+  override def prerequisites: Seq[TransformDependency] = Forms.MidForm ++
+    Seq( Dependency(firrtl.passes.LowerTypes),
+         Dependency(firrtl.transforms.RemoveReset),
+         Dependency[firrtl.transforms.RemoveWires] )
+
+  override def optionalPrerequisites = Seq(Dependency[firrtl.transforms.ConstantPropagation])
+
+  override def optionalPrerequisiteOf =
+    Seq( Dependency(firrtl.passes.memlib.VerilogMemDelays),
+      Dependency[SystemVerilogEmitter],
+      Dependency[VerilogEmitter] )
+
+  override def invalidates(a: Transform): Boolean = a match {
+    case _: firrtl.transforms.ConstantPropagation | firrtl.passes.Legalize => true
+    case _ => false
+  }
+
+  import firrtl.passes.PadWidths.forceWidth
+  private def getWidth(e: Expression): Int = bitWidth(e.tpe).toInt
+
+  // Recursive, updates expression so children exp's have correct widths
+  private def onExp(e: Expression): Expression = e.map(onExp) match {
+    case ex: DoPrim => ex.op match {
+      case Mul | Div | Rem => ex.map(forceWidth(ex.args.map(getWidth).max))
+      case Dshl =>
+        // special case as args aren't all same width
+        ex copy (op = Dshlw, args = Seq(forceWidth(getWidth(ex))(ex.args.head), ex.args(1)))
+      case _ => ex
+    }
+    case ex => ex
+  }
+
+  private def onStmt(s: Statement): Statement = s.map(onExp).map(onStmt)
+  def run(c: Circuit): Circuit = c.copy(modules = c.modules.map(_.map(onStmt)))
+}
--- src/main/scala/firrtl/passes/PadWidths.scala
+++ src/main/scala/firrtl/passes/PadWidths.scala
@@ -31,41 +31,42 @@ object PadWidths extends Pass {
     case _ => false
   }
 
-  private def width(t: Type): Int = bitWidth(t).toInt
-  private def width(e: Expression): Int = width(e.tpe)
-  // Returns an expression with the correct integer width
-  private def fixup(i: Int)(e: Expression) = {
-    def tx = e.tpe match {
-      case t: UIntType => UIntType(IntWidth(i))
-      case t: SIntType => SIntType(IntWidth(i))
-      // default case should never be reached
-    }
-    width(e) match {
-      case j if i > j => DoPrim(Pad, Seq(e), Seq(i), tx)
-      case j if i < j =>
-        val e2 = DoPrim(Bits, Seq(e), Seq(i - 1, 0), UIntType(IntWidth(i)))
-        // Bit Select always returns UInt, cast if selecting from SInt
-        e.tpe match {
-          case UIntType(_) => e2
-          case SIntType(_) => DoPrim(AsSInt, Seq(e2), Seq.empty, SIntType(IntWidth(i)))
-        }
-      case _ => e
+
+  /** Adds padding or a bit extract to ensure that the expression is of the with specified.
+    * @note only works on UInt and SInt type expressions, other expressions will yield a match error */
+  private[firrtl] def forceWidth(width: Int)(e: Expression): Expression = {
+    val old = getWidth(e)
+    if(width == old) { e }
+    else if(width > old) {
+      // padding retains the signedness
+      val newType = e.tpe match {
+        case _: UIntType => UIntType(IntWidth(width))
+        case _: SIntType => SIntType(IntWidth(width))
+        case other => throw new RuntimeException(s"forceWidth does not support expressions of type $other")
+      }
+      DoPrim(Pad, Seq(e), Seq(width), newType)
+    } else {
+      val e2 = DoPrim(Bits, Seq(e), Seq(width - 1, 0), UIntType(IntWidth(width)))
+      // Bit Select always returns UInt, cast if selecting from SInt
+      e.tpe match {
+        case UIntType(_) => e2
+        case SIntType(_) => DoPrim(AsSInt, Seq(e2), Seq.empty, SIntType(IntWidth(width)))
+      }
     }
   }
+
+  private def getWidth(t: Type): Int = bitWidth(t).toInt
+  private def getWidth(e: Expression): Int = getWidth(e.tpe)
 
   // Recursive, updates expression so children exp's have correct widths
   private def onExp(e: Expression): Expression = e map onExp match {
     case Mux(cond, tval, fval, tpe) =>
-      Mux(cond, fixup(width(tpe))(tval), fixup(width(tpe))(fval), tpe)
-    case ex: ValidIf => ex copy (value = fixup(width(ex.tpe))(ex.value))
+      Mux(cond, forceWidth(getWidth(tpe))(tval), forceWidth(getWidth(tpe))(fval), tpe)
+    case ex: ValidIf => ex copy (value = forceWidth(getWidth(ex.tpe))(ex.value))
     case ex: DoPrim => ex.op match {
-      case Lt | Leq | Gt | Geq | Eq | Neq | Not | And | Or | Xor |
-           Add | Sub | Mul | Div | Rem | Shr =>
+      case Lt | Leq | Gt | Geq | Eq | Neq | And | Or | Xor | Add | Sub =>
         // sensitive ops
-        ex map fixup((ex.args map width foldLeft 0)(math.max))
-      case Dshl =>
-        // special case as args aren't all same width
-        ex copy (op = Dshlw, args = Seq(fixup(width(ex.tpe))(ex.args.head), ex.args(1)))
+        ex.map(forceWidth(ex.args.map(getWidth).max))
       case _ => ex
     }
     case ex => ex
@@ -74,9 +75,9 @@ object PadWidths extends Pass {
   // Recursive. Fixes assignments and register initialization widths
   private def onStmt(s: Statement): Statement = s map onExp match {
     case sx: Connect =>
-      sx copy (expr = fixup(width(sx.loc))(sx.expr))
+      sx copy (expr = forceWidth(getWidth(sx.loc))(sx.expr))
     case sx: DefRegister =>
-      sx copy (init = fixup(width(sx.tpe))(sx.init))
+      sx copy (init = forceWidth(getWidth(sx.tpe))(sx.init))
     case sx => sx map onStmt
   }
 
