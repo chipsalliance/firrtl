6fbc97600fa8b200fb9e33c0e602cfe6676ceb8b
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2021-03-26 13:50:21 -0400

    Fix bug in zero-width memory removal (#2153)
    * Fix bug in zero-width memory removal
    
    Correctly remove all extraneous connections to all types of memory
    ports (read, write, readwrite) for zero-width memories.  Previously,
    only read ports were correctly handled.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>
    
    * fixup! Fix bug in zero-width memory removal
    
    (cherry picked from commit 67ce97a10564cfa07829af8cfce562009d60bafb)
    
    # Conflicts:
    #	src/main/scala/firrtl/passes/ZeroWidth.scala

--- src/main/scala/firrtl/passes/ZeroWidth.scala
+++ src/main/scala/firrtl/passes/ZeroWidth.scala
@@ -24,6 +24,7 @@ object ZeroWidth extends Transform with DependencyAPIMigration {
     case _          => false
   }
 
+<<<<<<< HEAD
   private def makeEmptyMemBundle(name: String): Field =
     Field(name, Flip, BundleType(Seq(
       Field("addr", Default, UIntType(IntWidth(0))),
@@ -43,6 +44,29 @@ object ZeroWidth extends Transform with DependencyAPIMigration {
       case Some(_) => d
     }
     case sx => sx map onEmptyMemStmt
+=======
+  private def makeZero(tpe: ir.Type): ir.Type = tpe match {
+    case ClockType => UIntType(IntWidth(0))
+    case a: UIntType      => a.copy(IntWidth(0))
+    case a: SIntType      => a.copy(IntWidth(0))
+    case a: AggregateType => a.map(makeZero)
+  }
+
+  private def onEmptyMemStmt(s: Statement): Statement = s match {
+    case d @ DefMemory(info, name, tpe, _, _, _, rs, ws, rws, _) =>
+      removeZero(tpe) match {
+        case None =>
+          DefWire(
+            info,
+            name,
+            MemPortUtils
+              .memType(d)
+              .map(makeZero)
+          )
+        case Some(_) => d
+      }
+    case sx => sx.map(onEmptyMemStmt)
+>>>>>>> 67ce97a1... Fix bug in zero-width memory removal (#2153)
   }
 
   private def onModuleEmptyMemStmt(m: DefModule): DefModule = {
