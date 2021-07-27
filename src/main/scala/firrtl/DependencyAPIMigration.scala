e45c4be4984b8758716a294459eb02c49440315c
Jiuyang Liu <liu@jiuyang.me>
2021-07-27 11:53:19 +0800

    Update DependencyAPIMigration.scala

--- src/main/scala/firrtl/DependencyAPIMigration.scala
+++ src/main/scala/firrtl/DependencyAPIMigration.scala
@@ -16,7 +16,7 @@ import firrtl.stage.TransformManager.TransformDependency
   * For more information, see: https://bit.ly/2Voppre
   */
 @deprecated(
-  "Please remove DependencyAPIMigration from your Transform to be compatible with FIRRTL 1.6."
+  "Please remove DependencyAPIMigration from your Transform to be compatible with FIRRTL 1.6.",
   "FIRRTL 1.5"
 )
 trait DependencyAPIMigration { this: Transform =>
