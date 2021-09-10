7dbabb6ff8f623831614bbb8a84829f037f6888a
Megan Wachs <megan@sifive.com>
2021-09-09 22:46:15 -0700

    MemConf: scalafmt for the tests

--- src/test/scala/firrtlTests/ReplSeqMemTests.scala
+++ src/test/scala/firrtlTests/ReplSeqMemTests.scala
@@ -51,8 +51,10 @@ class ReplSeqMemSpec extends SimpleTransformSpec {
     val fromConf = MemConf.fromString(text)
     // Verify that the number of lines in the Conf file is equivalent to the number
     // of entries (no missing or extra newlines)
-    require(fromConf.size == text.count(_ == '\n'),
-      s"Unexpected formatting in mem.conf file containing ${fromConf.size} confs:\n${text}")
+    require(
+      fromConf.size == text.count(_ == '\n'),
+      s"Unexpected formatting in mem.conf file containing ${fromConf.size} confs:\n${text}"
+    )
     // Verify the mems in the conf are the same as the expected ones
     require(
       Set(fromConf: _*) == mems,
