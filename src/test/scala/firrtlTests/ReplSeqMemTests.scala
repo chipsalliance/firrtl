eb98cea25e0c3d21bb90f9981360ea65e5ac920e
Megan Wachs <megan@sifive.com>
2021-09-09 22:29:18 -0700

    Update ReplSeqMemTests.scala

--- src/test/scala/firrtlTests/ReplSeqMemTests.scala
+++ src/test/scala/firrtlTests/ReplSeqMemTests.scala
@@ -52,7 +52,7 @@ class ReplSeqMemSpec extends SimpleTransformSpec {
     // Verify that the number of lines in the Conf file is equivalent to the number
     // of entries (no missing or extra newlines)
     require(fromConf.size == text.count(_ == '\n'),
-      s"Unexpected formatting in mem.conf file containing ${fromConf.size} confs:\n${text}")
+      s"Unexpected newlines in mem.conf file containing ${fromConf.size} confs:\n${text}")
     // Verify the mems in the conf are the same as the expected ones
     require(
       Set(fromConf: _*) == mems,
