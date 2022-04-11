a4e50ca75867a82bfae37856631c86595d634044
Jack Koenig <koenig@sifive.com>
2022-04-11 16:25:13 -0700

    Resolve backport conflicts

--- src/main/scala/firrtl/package.scala
+++ src/main/scala/firrtl/package.scala
@@ -10,16 +10,9 @@ package object firrtl {
   implicit def annoSeqToSeq(as: AnnotationSeq): Seq[Annotation] = as.underlying
 
   /* Options as annotations compatibility items */
-<<<<<<< HEAD
-  @deprecated("Use firrtl.stage.TargetDirAnnotation", "1.2")
-  type TargetDirAnnotation = firrtl.options.TargetDirAnnotation
-
-  @deprecated("Use firrtl.stage.TargetDirAnnotation", "1.2")
-=======
   @deprecated("Use firrtl.options.TargetDirAnnotation", "FIRRTL 1.2")
   type TargetDirAnnotation = firrtl.options.TargetDirAnnotation
 
   @deprecated("Use firrtl.options.TargetDirAnnotation", "FIRRTL 1.2")
->>>>>>> 06ccf51a (Fix incorrect deprecation warning for TargetDirAnnotation (#2511))
   val TargetDirAnnotation = firrtl.options.TargetDirAnnotation
 }
